dbdb323dd432ffd5ab40e256833af2d803daa279
Josh Chappelow <jdchappelow@gmail.com>
2022-12-14 23:27:41 +0900

    build: initial build and setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+
+//element is the new syntax
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Routes>
+      <Route path="/welcome" element={<Welcome/>}/>
+      <Route path="/products" element={<Products/>}/>
+    </Routes>
     </div>
   );
 }
---
+++ src/pages/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Welcome(props) {
+  return (
+    <div>
+      Hello welcome page
+    </div>
+  );
+}
+
+export default Welcome;
