22f5e87179c51c24271786610593cbf1dd6b2fdd
Josh Chappelow <jdchappelow@gmail.com>
2022-12-22 12:07:07 +0900

    feat: placeholder product page and styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
 
 //element is the new syntax
 
 function App() {
   return (
     <div>
-    <Routes>
-      <Route path="/welcome" element={<Welcome/>}/>
-      <Route path="/products" element={<Products/>}/>
-    </Routes>
+      <MainHeader/>
+      <main>
+        <Routes>
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/components/MainHeader.js
@@ -0,0 +1,30 @@
+import { NavLink } from 'react-router-dom';
+
+import classes from './MainHeader.module.css';
+
+
+const classNameFunc = ({ isActive }) => (isActive ? classes.nav_active : "");
+
+
+const MainHeader = () => {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul>
+          <li>
+            <NavLink className={classNameFunc} to='/welcome'>
+              Welcome
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={classNameFunc} to='/products'>
+              Products
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
